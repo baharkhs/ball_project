4dea77f75798eb66d11c2e549f03be6e8e0a1d26
bahark97 <baharshaghaghi76@gmail.com>
2025-02-19 11:37:33 +0100

    19.2 part2

--- examples/TEST OH.py
+++ examples/TEST OH.py
@@ -9,9 +9,14 @@ sim = Simulation(well_radius=2.0, well_height=2.0, total_time=5.0, dt=0.001)
 initial_velocity = [1.2, 1.2, 1.2]
 
 # Create one water molecule (1 O and 2 H) with molecule_id "H2O".
-sim.create_water_molecule(center_position=[0.0, 0.0, 1.0],
+sim.create_water_molecule(center_position=[0.0, 0.0, 1.5],
                           velocity=initial_velocity,
                           molecule_id="H2O")
+
+# Create a second water molecule.
+sim.create_water_molecule(center_position=[0.0, 0.0, 0.5],
+                          velocity=initial_velocity,
+                          molecule_id="H2O2")
 
 # Instantiate the plotter and run the animation and additional plots.
 plotter = SimulationPlotter(sim)
